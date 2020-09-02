420388bcd60c69b83aa730657bf6790cbf731898
folksfolk <dhaniiihh@gmail.com>
2020-09-02 23:41:15 +0700

    tambah tugas

--- index2.js
+++ index2.js
@@ -8,8 +8,8 @@ var books = [
 
 function buku() {
     readBooksPromise(10000, books[0])
-        .then((time)=>{readBooksPromise(time, books[1]);})
-        .then((time)=>{readBooksPromise(time, books[2]);})
+        .then((time)=>{return readBooksPromise(time, books[1]);})
+        .then((time)=>{return readBooksPromise(time, books[2]);})
         .catch((time) => console.log(time));
 }
 
